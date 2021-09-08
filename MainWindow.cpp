2aa4d35fd156f6f962b4e656f73e644024527c7e
Grzegorz Dziubak <grzegorzdziubak@gmail.com>
2021-09-08 21:44:50 +0200

    Some first methods

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -6,10 +6,36 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    init();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    close();
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    if (importIsRunning)
+        event->ignore();
+    else
+    {
+        DAOLib::closeConnection();
+        event->accept();
+    }
+
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    if (event->key() == Qt::Key_Escape)
+        stopImport = true;
+
 }
 
--- MainWindow.h
+++ MainWindow.h
@@ -1,7 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QLabel>
 #include <QMainWindow>
+#include <QProgressBar>
+#include <QTranslator>
+#include <QCloseEvent>
+#include <QKeyEvent>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +21,28 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+
+private slots:
+    void on_actionExit_triggered();
+
 private:
     Ui::MainWindow *ui;
+
+    QTranslator *translator;
+    QLabel *statuslabel;
+    QProgressBar *progressBar;
+
+    void closeEvent(QCloseEvent *event) override;
+    void keyPressEvent(QKeyEvent *event) override;
+
+    void init();
+    bool importIsRunning;
+    bool stopImport;
+
+
+
+
+
+    Q_PROPERTY(QTranslator * translator READ getTranslator CONSTANT)
 };
 #endif // MAINWINDOW_H
