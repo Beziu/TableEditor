a8fa85819b35947eb1d56e04d4978c900d25b49f
Grzegorz Dziubak <grzegorzdziubak@gmail.com>
2021-09-11 21:37:26 +0200

    Ney read query add

--- PostCodesDAO.cpp
+++ PostCodesDAO.cpp
@@ -30,6 +30,7 @@ bool PostCodesDAO::postCodeExists(const QString &code, const QString &city)
         return false;
     else
         return count.toInt() > 0;
+
 }
 
 void PostCodesDAO::deleteTable()
@@ -52,6 +53,47 @@ int PostCodesDAO::getRowCount()
         return 0;
     else
         return count.toInt();
+
+}
+
+QSqlTableModel *PostCodesDAO::readPostCodeInToTableModel()
+{
+    QSqlTableModel *tableModel = new QSqlTableModel(nullptr, DAOLib::getDatabaseConnection());
+
+    tableModel->setTable("PostCodes");
+    tableModel->setEditStrategy(QSqlTableModel::OnManualSubmit);
+    tableModel->select();
+
+    return tableModel;
+
+}
+
+PostCode *PostCodesDAO::readPostCode(qint64 key)
+{
+    PostCode *code = nullptr;
+    bool ok;
+
+    QString sql = "SELECT PRIMARYKEY, CODE, CITY, TIMESTAMP FROM POSTCODES ";
+    sql += "WHERE PRIMARYKEY = " + QString::number(key);
+
+    QSqlQuery *query = DAOLib::executeQuery(sql, ok);
+
+    if (!ok)
+    {
+        delete query;
+        return code;
+    }
+
+    query->first();
+
+    code = new PostCode();
+    code->setPrimKey(query->value("PRIMARYKEY").toLongLong());
+    code->setCode(query->value("CODE").toString());
+    code->setCity(query->value("CITY").toString());
+    code->setTimeStamp(query->value("TIMESTAMP").toString());
+
+    delete query;
+    return code;
 
 }
 
--- PostCodesDAO.h
+++ PostCodesDAO.h
@@ -1,17 +1,27 @@
 #ifndef POSTCODESDAO_H
 #define POSTCODESDAO_H
 
+#include <QObject>
+#include <QtWidgets>
+#include <QSqlTableModel>
+
 #include "DAOLib.h"
+#include "PostCode.h"
 
 class PostCodesDAO
 {
 public:
     static bool insertPostCode(const QString &code, const QString &city);
     static bool postCodeExists(const QString &code, const QString &city);
-    static void deleteTable();
-    static int getRowCount();
+    static bool updatePostCode(qint64 key, const QString &code, const QString &city);
     static bool deletePostCode(qint64 key);
+    static void deleteTable();
 
+    static int getRowCount();
+    static qint64 getLastIdentity();
+
+    static QSqlTableModel *readPostCodeInToTableModel();
+    static PostCode *readPostCode(qint64 key);
 
 private:
     PostCodesDAO();
