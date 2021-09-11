7c64f3e5aa4b322b6eb26c3bd39ec52849a4960c
Grzegorz Dziubak <grzegorzdziubak@gmail.com>
2021-09-11 21:38:30 +0200

    New functions

--- PostCodesWindow.cpp
+++ PostCodesWindow.cpp
@@ -1,14 +1,71 @@
-#include "PostCodes.h"
-#include "ui_PostCodes.h"
+#include "PostCodesWindow.h"
+#include "ui_PostCodesWindow.h"
+
+#include "PostCodesDAO.h"
 
 PostCodes::PostCodes(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::PostCodes)
 {
     ui->setupUi(this);
+
+    init();
 }
 
 PostCodes::~PostCodes()
 {
     delete ui;
 }
+
+void PostCodes::init()
+{
+
+}
+
+void PostCodes::showTable()
+{
+    QSqlTableModel *model = setTableViewModel();
+}
+
+QSqlTableModel *PostCodes::setTableViewModel()
+{
+    delete ui->tableView->model();
+
+    QSqlTableModel *model = PostCodesDAO::readPostCodeInToTableModel();
+
+    model->sort(model->record().indexOf("CODE"), Qt::SortOrder::AscendingOrder);
+
+
+
+
+
+}
+
+void PostCodes::deleteEntry(const QModelIndex index)
+{
+
+}
+
+
+void PostCodes::on_actionNew_triggered()
+{
+
+}
+
+void PostCodes::on_actionEdit_triggered()
+{
+
+}
+
+void PostCodes::on_actionDelete_triggered()
+{
+
+}
+
+void PostCodes::on_actionEXit_triggered()
+{
+    close();
+}
+
+
+
--- PostCodesWindow.h
+++ PostCodesWindow.h
@@ -2,6 +2,11 @@
 #define POSTCODESWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QSqlTableModel>
+#include <QSqlRecord>
+#include <QVariant>
+#include <QKeyEvent>
 
 namespace Ui {
 class PostCodes;
@@ -15,8 +20,28 @@ public:
     explicit PostCodes(QWidget *parent = nullptr);
     ~PostCodes();
 
+private slots:
+    void on_actionEXit_triggered();
+
+    void on_actionNew_triggered();
+
+    void on_actionEdit_triggered();
+
+    void on_actionDelete_triggered();
+
 private:
     Ui::PostCodes *ui;
+
+    QLabel *statusLabel;
+
+    void init();
+    void showTable();
+    void deleteEntry(const QModelIndex index);
+
+    QSqlTableModel *setTableViewModel();
+
+
+
 };
 
 #endif // POSTCODESWINDOW_H
